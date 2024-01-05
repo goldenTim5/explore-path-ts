8ec45eb280ab197d3ff3aab196a8ac72c6584f83
GoldenTim <tim.gf3825@outlook.com>
2024-01-05 10:48:13 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import Grid from "./components/Grid/Grid";
+
+function App() {
+	const navbarHeight = 150;
+	const textHeight = 60;
+	const [dimensions, setDimensions] = useState({ height: 0, width: 0 });
+
+	useEffect(() => {
+		const calculateDimensions = () => {
+			const height = Math.floor(
+				(window.innerHeight - navbarHeight - textHeight) / 28
+			);
+			const width = Math.floor(window.innerWidth / 25);
+			setDimensions({ height, width });
+		};
+
+		calculateDimensions();
+
+		window.addEventListener("resize", calculateDimensions);
+
+		return () => window.removeEventListener("resize", calculateDimensions);
+	}, []);
+
+	return (
+		<>
+			<Grid numRows={dimensions.height} numCols={dimensions.width} />
+		</>
+	);
+}
+
+export default App;
