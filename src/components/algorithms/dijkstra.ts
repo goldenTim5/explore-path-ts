f8628a850a438b002d724119e14a1bd9279d6f02
thomasL <thomas.el.8258@gmail.com>
2024-01-07 17:28:15 +0000

    dijstra working

--- src/components/algorithms/dijkstra.ts
+++ src/components/algorithms/dijkstra.ts
@@ -25,7 +25,7 @@ export function disjktraAlgorithm(
 		// returns the shortest path
 		if (currentNode.status === "target") {
 			while (currentNode !== null) {
-				shortestPath.unshift(currentNode);
+				shortestPath.push(currentNode);
 				currentNode = currentNode.parentNode;
 			}
 			break;
