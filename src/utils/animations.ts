720505921956b9ca48d2b5dc8c1fe4bc62c2a26c
GoldenTim <tim.gf3825@outlook.com>
2024-01-14 14:26:45 +0000

    refractor animations

--- src/utils/animations.ts
+++ src/utils/animations.ts
@@ -1,7 +1,8 @@
 import gsap from "gsap";
 
-const visitedAnimationTime = 5;
-const shortPathAnimationTime = 20; // this number has to be bigger than the above
+const visitedAnimationTime = 10;
+const shortPathAnimationTime = 50; // this number has to be bigger than the above
+const keepVisitedAnimation = true;
 
 interface NodePosition {
 	x: number;
@@ -12,6 +13,18 @@ export interface NodeState {
 	position: NodePosition;
 	status: string;
 	parentNode: NodeState | null;
+}
+
+function animateNode(nodeId: string, className: string): void {
+	// add animation class to each node
+	const nodeRef = document.getElementById(nodeId);
+	if (nodeRef) {
+		gsap.to(nodeRef, {
+			onComplete: () => {
+				nodeRef.className = className;
+			},
+		});
+	}
 }
 
 export function animatePathfiding(
@@ -28,22 +41,13 @@ export function animatePathfiding(
 		if (node.status === "unvisited") {
 			setTimeout(() => {
 				const nodeId = `node-${node.position.x}-${node.position.y}`;
-				animateNode(nodeId, "node visited");
+				animateNode(
+					nodeId,
+					`node ${keepVisitedAnimation ? "keep-visited" : "visited"}`
+				);
 			}, visitedAnimationTime * index);
 		}
 	});
-}
-
-function animateNode(nodeId: string, className: string): void {
-	// add animation class to each node
-	const nodeRef = document.getElementById(nodeId);
-	if (nodeRef) {
-		gsap.to(nodeRef, {
-			onComplete: () => {
-				nodeRef.className = className;
-			},
-		});
-	}
 }
 
 function shortestPathAnimation(shortestPath: NodeState[]): void {
